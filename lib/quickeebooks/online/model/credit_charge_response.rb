3e4df5f82216ba6a6c719f0ddfe555034ea13a6b
David Raphael <draphael@iscanonline.com>
2013-11-01 11:52:58 -0500

    added missing fields for recon data

--- lib/quickeebooks/online/model/credit_charge_response.rb
+++ lib/quickeebooks/online/model/credit_charge_response.rb
@@ -7,6 +7,8 @@ module Quickeebooks
         xml_accessor :status,                 :from => 'Status'
         xml_accessor :auth_code,              :from => 'AuthCode'
         xml_accessor :avs_street,             :from => 'AvsStreet'
+        xml_accessor :recon_batch_id,        :from => 'ReconBatchId'
+        xml_accessor :payment_grouping_code, :from => 'PaymentGroupingCode'
       end
     end
   end
