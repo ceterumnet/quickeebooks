3aef4c5ba14c674f6454419bffb8c92c89726fea
David Raphael <draphael@iscanonline.com>
2013-11-01 11:54:37 -0500

    Added merchant account number

--- lib/quickeebooks/online/model/credit_charge_response.rb
+++ lib/quickeebooks/online/model/credit_charge_response.rb
@@ -3,12 +3,13 @@ module Quickeebooks
     module Model
       class CreditChargeResponse  < Quickeebooks::Online::Model::IntuitType
         xml_name 'CreditChargeResponse'
-        xml_accessor :cc_transaction_id,      :from => 'CCTransId'
-        xml_accessor :status,                 :from => 'Status'
-        xml_accessor :auth_code,              :from => 'AuthCode'
-        xml_accessor :avs_street,             :from => 'AvsStreet'
-        xml_accessor :recon_batch_id,        :from => 'ReconBatchId'
-        xml_accessor :payment_grouping_code, :from => 'PaymentGroupingCode'
+        xml_accessor :cc_transaction_id,       :from => 'CCTransId'
+        xml_accessor :status,                  :from => 'Status'
+        xml_accessor :auth_code,               :from => 'AuthCode'
+        xml_accessor :avs_street,              :from => 'AvsStreet'
+        xml_accessor :recon_batch_id,          :from => 'ReconBatchId'
+        xml_accessor :payment_grouping_code,   :from => 'PaymentGroupingCode'
+        xml_accessor :merchant_account_number, :from => 'MerchantAccountNumber'
       end
     end
   end
