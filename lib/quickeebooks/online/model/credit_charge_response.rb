c2aee101a6310bce8bbcc15b373c9fcf45d764cf
David Raphael <draphael@iscanonline.com>
2013-11-01 12:23:22 -0500

    Added other fields for credit charge response

--- lib/quickeebooks/online/model/credit_charge_response.rb
+++ lib/quickeebooks/online/model/credit_charge_response.rb
@@ -3,14 +3,18 @@ module Quickeebooks
     module Model
       class CreditChargeResponse  < Quickeebooks::Online::Model::IntuitType
         xml_name 'CreditChargeResponse'
-        xml_accessor :cc_transaction_id,       :from => 'CCTransId'
-        xml_accessor :status,                  :from => 'Status'
-        xml_accessor :auth_code,               :from => 'AuthCode'
-        xml_accessor :avs_street,              :from => 'AvsStreet'
-        xml_accessor :recon_batch_id,          :from => 'ReconBatchId'
-        xml_accessor :payment_grouping_code,   :from => 'PaymentGroupingCode'
-        xml_accessor :merchant_account_number, :from => 'MerchantAccountNumber'
-        xml_accessor :txn_authorization_stamp, :from => 'TxnAuthorizationStamp'
+        xml_accessor :cc_transaction_id,        :from => 'CCTransId'
+        xml_accessor :status,                   :from => 'Status'
+        xml_accessor :result_code,              :from => 'ResultCode'
+        xml_accessor :result_msg,               :from => 'ResultMsg'
+        xml_accessor :merchant_account_number,  :from => 'MerchantAccountNumber'
+        xml_accessor :auth_code,                :from => 'AuthCode'
+        xml_accessor :avs_street,               :from => 'AvsStreet'
+        xml_accessor :recon_batch_id,           :from => 'ReconBatchId'
+        xml_accessor :payment_grouping_code,    :from => 'PaymentGroupingCode'
+        xml_accessor :txn_authorization_time,   :from => 'TxnAuthorizationTime'
+        xml_accessor :txn_authorization_stamp,  :from => 'TxnAuthorizationStamp'
+        xml_accessor :client_trans_id,          :from => 'ClientTransID'
       end
     end
   end
